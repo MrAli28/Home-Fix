96d2e6a28029f658faf18963bbdc9e18fe9f1094
MrAli28 <huss133236@gmail.com>
2026-04-04 21:19:16 +0500

    Handle favicon requests without server errors

--- homeservices/config/urls.py
+++ homeservices/config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', lambda request: HttpResponse(status=204)),
     path('', include('services.urls')),  # Include our app's URLs
 ]
 
